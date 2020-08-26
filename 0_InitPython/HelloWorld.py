d7e04eeddd112489d2c60ef445633822644beb1c
Andrea Heilrath <andrea.heilrath@gmail.com>
2020-08-26 17:11:35 +0200

    upload basic materials from last semester

---
+++ 0_InitPython/HelloWorld.py
@@ -0,0 +1,13 @@
+import math
+
+var1 = "Hello World"
+
+#print(var1)
+#print(math.pi)
+
+var2 = 5
+
+for zaehler in range(0,10): #hier nimmt der zaehler die werte 0 ... 9 an
+	print(var2**zaehler)
+
+print(var1, var2)
