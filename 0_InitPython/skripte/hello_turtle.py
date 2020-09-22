b7adc19ddf429661d5459a826601e01b8857178b
Andrea Heilrath <andrea.heilrath@gmail.com>
2020-09-22 23:14:57 +0200

    turtle improvements

--- 0_InitPython/skripte/hello_turtle.py
+++ 0_InitPython/skripte/hello_turtle.py
@@ -1,6 +1,7 @@
 import turtle
-for zaehler in range(0,38):
-  turtle.forward(10)
-  turtle.left(10)
+for step in range(0,37):
+	print("Step no.", step)
+	turtle.forward(10)
+	turtle.left(10)
 
 turtle.mainloop()
